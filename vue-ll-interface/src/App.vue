3cd7850fa6e404c491f1c8b409d3b16fac4e94d8
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-04-09 16:39:20 +0200

    creates axios request to get data from json file

--- vue-ll-interface/src/App.vue
+++ vue-ll-interface/src/App.vue
@@ -25,53 +25,30 @@
 
 <!-- App State -->
 <script>
-export default {
-  name: "main-app",
-  // --- data ---
-  data: function () {
-    return {
-      title: "Current Appointments:",
+  import axios from "axios";
 
-      appointments: [
-        {
-          petName: "Pepe",
-          ownerName: "Reggie Tupp",
-          aptNotes: "It's time for this rabbit's post spaying surgery checkup",
-          aptDate: "2018-11-28 13:30",
-        },
-        {
-          petName: "Rio",
-          ownerName: "Philip Ransu",
-          aptNotes: "Rio is up for his next round of vaccinations",
-          aptDate: "2018-11-28 10:15",
-        },
-        {
-          petName: "Scooter",
-          ownerName: "Zachary Heilyn",
-          aptNotes:
-            "Scooter has been pawing at his ear and may have an ear infection",
-          aptDate: "2018-11-28 14:45",
-        },
-        {
-          petName: "Nadalee",
-          ownerName: "Krystle Valerija",
-          aptNotes:
-            "This dog is coming in for his monthly nail trim and grooming",
-          aptDate: "2018-11-28 16:00",
-        },
-        {
-          petName: "Scout",
-          ownerName: "Nicolette Bardeau",
-          aptNotes:
-            "This dog is coming in for his annual checkup and vaccinations",
-          aptDate: "2018-11-28 9:00",
-        },
-      ],
-    };
-  },
-  // --- components ---
-  components: {},
-};
+  export default {
+
+    // -- template --
+    name: "main-app",
+    
+    // -- data --
+    data: function () {
+      return {
+        title: "Current Appointments:",
+        appointments: []
+      };
+    },
+
+    // -- components --
+    components: {},
+
+    // -- lifecycle hooks --
+    mounted() {
+      axios.get("../data/appointments.json")
+        .then( response => (this.appointments = response.data) )
+    }
+  };
 </script>
 
 
